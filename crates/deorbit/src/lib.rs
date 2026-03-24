39e8825040655d0325f94f52460e1f86cc32ba93
hermanest <mutcmk@gmail.com>
2026-03-24 20:10:56 +0100

    Removed redundant test crate wrapper.

--- crates/deorbit/src/lib.rs
+++ crates/deorbit/src/lib.rs
@@ -2,11 +2,13 @@ mod arc;
 mod builder;
 mod factory;
 mod services;
-mod test;
 mod binding;
 mod from_di;
 mod graph;
 mod error;
+
+#[cfg(test)]
+mod test;
 
 pub use builder::{ServicesBuilder};
 pub use services::{Service, Services};
--- crates/deorbit/src/test.rs
+++ crates/deorbit/src/test.rs
@@ -1,82 +1,77 @@
+use crate::Service;
 use crate::builder::ServicesBuilder;
 use crate::error::Error;
 use crate::from_di::FromDi;
-use crate::Service;
 use deorbit_macro::FromDi;
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn binds_concrete() -> Result<(), Error> {
-        #[derive(FromDi)]
-        struct Foo {
-            a: Service<i32>,
-        }
-
-        #[derive(FromDi)]
-        struct Bar {
-            a: Service<Foo>,
-        }
-
-        let mut builder = ServicesBuilder::new();
-
-        builder.bind_singleton_from(10i32);
-        builder.bind_singleton::<Bar>();
-        builder.bind_singleton::<Foo>();
-
-        let services = builder.build()?;
-
-        services.resolve::<Foo>().expect("Failed to resolve Foo");
-        services.resolve::<Bar>().expect("Failed to resolve Bar");
-
-        Ok(())
+#[test]
+fn binds_concrete() -> Result<(), Error> {
+    #[derive(FromDi)]
+    struct Foo {
+        a: Service<i32>,
     }
 
-    #[test]
-    fn fails_duplicated() {
-        let mut builder = ServicesBuilder::new();
-
-        builder.bind_singleton_from(10i32);
-        builder.bind_singleton_from(10i32);
-
-        let res = builder.build();
-
-        assert!(matches!(res, Err(Error::Duplicated { .. })));
+    #[derive(FromDi)]
+    struct Bar {
+        a: Service<Foo>,
     }
 
-    #[test]
-    fn fails_missing() {
-        let mut builder = ServicesBuilder::new();
+    let mut builder = ServicesBuilder::new();
 
-        builder.bind_singleton_from(10i32);
-        builder.bind_singleton_from::<i128, _, _>(|_: Service<i64>| 0i128);
+    builder.bind_singleton_from(10i32);
+    builder.bind_singleton::<Bar>();
+    builder.bind_singleton::<Foo>();
 
-        let res = builder.build();
+    let services = builder.build()?;
 
-        assert!(matches!(res, Err(Error::Missing { .. })));
+    services.resolve::<Foo>().expect("Failed to resolve Foo");
+    services.resolve::<Bar>().expect("Failed to resolve Bar");
+
+    Ok(())
+}
+
+#[test]
+fn fails_duplicated() {
+    let mut builder = ServicesBuilder::new();
+
+    builder.bind_singleton_from(10i32);
+    builder.bind_singleton_from(10i32);
+
+    let res = builder.build();
+
+    assert!(matches!(res, Err(Error::Duplicated { .. })));
+}
+
+#[test]
+fn fails_missing() {
+    let mut builder = ServicesBuilder::new();
+
+    builder.bind_singleton_from(10i32);
+    builder.bind_singleton_from::<i128, _, _>(|_: Service<i64>| 0i128);
+
+    let res = builder.build();
+
+    assert!(matches!(res, Err(Error::Missing { .. })));
+}
+
+#[test]
+fn fails_circular() {
+    #[derive(FromDi)]
+    struct BarCirc {
+        a: Service<FooCirc>,
     }
 
-    #[test]
-    fn fails_circular() {
-        #[derive(FromDi)]
-        struct BarCirc {
-            a: Service<FooCirc>,
-        }
-
-        #[derive(FromDi)]
-        struct FooCirc {
-            a: Service<BarCirc>,
-        }
-
-        let mut builder = ServicesBuilder::new();
-
-        builder.bind_singleton::<FooCirc>();
-        builder.bind_singleton::<BarCirc>();
-
-        let res = builder.build();
-
-        assert!(matches!(res, Err(Error::Circular { .. })));
+    #[derive(FromDi)]
+    struct FooCirc {
+        a: Service<BarCirc>,
     }
+
+    let mut builder = ServicesBuilder::new();
+
+    builder.bind_singleton::<FooCirc>();
+    builder.bind_singleton::<BarCirc>();
+
+    let res = builder.build();
+
+    assert!(matches!(res, Err(Error::Circular { .. })));
 }
